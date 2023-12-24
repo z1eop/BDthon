b2bef26d3df910b543815df86de11e0d7c191798
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-24 05:13:11 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Afrotoomusic
+![Shahm-Music](https://telegra.ph/file/1105de1e03f0ba27ba095.jpg )
+# afrotoo
---
+++ config.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/1105de1e03f0ba27ba095.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/1105de1e03f0ba27ba095.jpg")
+
+SESSION = getenv("SESSION", None)
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SORS0C")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/T_Y_E_X")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5904216848").split()))
+
+
+FAILED = "https://telegra.ph/file/1105de1e03f0ba27ba095.jpg"
