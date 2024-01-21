ff39d164206021f43e5b295f285f6475a479fc44
kazmabbas <105518597+kazmabbas@users.noreply.github.com>
2024-01-21 20:32:39 +0300

    Update __init__.py

--- ShahmMusic/__init__.py
+++ ShahmMusic/__init__.py
@@ -71,8 +71,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("CB_LQ")
-        await app2.join_chat("T_Y_E_X")
+        await app2.join_chat("BDthon")
+        await app2.join_chat("BDthon1")
     except:
         pass
 
